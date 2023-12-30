009ef362824f23694aae6ac090a749e57cabda59
mariusz.matkowski <mariusz.matkowski@yahoo.com>
2023-12-30 20:43:19 +0100

    [Client] 1) ScreenState class was added. 2) Function to create status_bar was added

--- src/client.cpp
+++ src/client.cpp
@@ -3,11 +3,57 @@
 
 #define ctrl(x) (x & 0x1F)
 
+const short REGULAR_PAIR = 0;
+const short HIGHLIGHT_PAIR = 1;
+
+class ScreenState {
+public:
+    ScreenState() = default;
+
+    ScreenState &operator=(const ScreenState &) = delete;
+    ScreenState &operator=(ScreenState &&) = delete;
+    ScreenState(const ScreenState &) = delete;
+    ScreenState(ScreenState &&) = delete;
+
+    static ScreenState enable() {
+        initscr();
+        noecho();
+        curs_set(0); // Cursor invisible
+        raw();
+        start_color();
+        return ScreenState{};
+    }
+
+    ~ScreenState() { endwin(); }
+};
+
+void status_bar(const std::string &label, int x, int y, int width,
+                short color_pair) {
+    auto n = label.length() - 1;
+    auto WHITE_SPACE_N = width - n - 1;
+
+    std::string spaces(WHITE_SPACE_N, ' ');
+
+    attron(COLOR_PAIR(color_pair));
+    move(y, x);
+    printw(label.c_str(), "%s");
+    printw(spaces.c_str(), "%s");
+
+    attroff(COLOR_PAIR(color_pair));
+}
+
 int main() {
-    initscr();
-    raw();
-    noecho();
-    std::cout << "Hello from client\n";
+    auto screen_state = ScreenState::enable();
+
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
+    int x, y;
+    getmaxyx(stdscr, y, x);
+
+    status_bar("CppChat", 0, 0, x, HIGHLIGHT_PAIR);
+
+    status_bar("Offline", 0, y - 2, x, HIGHLIGHT_PAIR);
 
     bool quit = false;
 
