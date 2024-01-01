b5139079b65b8c0ef1c12096bc0a85c99ae37c48
mariusz.matkowski <mariusz.matkowski@yahoo.com>
2024-01-01 08:42:33 +0100

    Basic chat added

--- src/client.cpp
+++ src/client.cpp
@@ -1,51 +1,124 @@
+#include <cctype>
 #include <iostream>
 #include <ncurses.h>
+#include <vector>
 
 #include "ScreenState/ScreenState.hpp"
 
 #define ctrl(x) (x & 0x1F)
 
-const short REGULAR_PAIR = 0;
-const short HIGHLIGHT_PAIR = 1;
+enum Color : short {
+    Regular = 0,
+    Highlight,
+};
+
+class Prompt {
+public:
+    void put(char ch) { data_.push_back(ch); }
+
+    void clear() { data_.clear(); }
+
+    std::string getPromptString() const {
+        return {std::begin(data_), std::end(data_)};
+    }
+
+    void render(int x, int y, int width) {
+        auto start_x = data_.size() + x;
+        auto WHITE_SPACE_N = width - start_x;
+
+        std::string spaces(WHITE_SPACE_N, ' ');
+
+        attron(COLOR_PAIR(Color::Regular));
+        if (!data_.empty()) {
+            move(y, x);
+            printw(std::string(std::begin(data_), std::end(data_)).c_str(),
+                   "%s");
+        }
+        move(y, start_x);
+        printw(spaces.c_str(), "%s");
+        attroff(COLOR_PAIR(Color::Regular));
+    }
+
+private:
+    std::vector<char> data_;
+};
+
+class ChatLog {
+public:
+    void put(std::string message, Color color = Color::Regular) {
+        items_.push_back(std::make_pair(std::move(message), color));
+    }
+
+    void render(int x, int y) {
+        for (unsigned offset = 0; const auto& [message, color] : items_) {
+            attron(COLOR_PAIR(color));
+            move(y + offset, x);
+            printw(message.c_str(), "%s");
+            attroff(COLOR_PAIR(color));
+            offset++;
+        }
+    }
+
+private:
+    std::vector<std::pair<std::string, Color>> items_;
+};
+
+struct Client {
+    ChatLog chat;
+    bool quit = false;
+};
 
 void status_bar(const std::string& label, int x, int y, int width,
-                short color_pair) {
+                Color color) {
     auto n = label.length() - 1;
     auto WHITE_SPACE_N = width - n - 1;
 
     std::string spaces(WHITE_SPACE_N, ' ');
 
-    attron(COLOR_PAIR(color_pair));
+    attron(COLOR_PAIR(color));
     move(y, x);
     printw(label.c_str(), "%s");
     printw(spaces.c_str(), "%s");
 
-    attroff(COLOR_PAIR(color_pair));
+    attroff(COLOR_PAIR(color));
 }
 
 int main() {
-    auto screen_state = ScreenState::enable();
+    Client client{};
+    Prompt prompt{};
 
-    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
-    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+    auto _screen_state = ScreenState::enable();
+
+    init_pair(Color::Regular, COLOR_WHITE, COLOR_BLACK);
+    init_pair(Color::Highlight, COLOR_BLACK, COLOR_WHITE);
 
     int x, y;
     getmaxyx(stdscr, y, x);
 
-    status_bar("CppChat", 0, 0, x, HIGHLIGHT_PAIR);
+    status_bar("CppChat", 0, 0, x, Color::Highlight);
 
-    status_bar("Offline", 0, y - 2, x, HIGHLIGHT_PAIR);
+    status_bar("Offline", 0, y - 2, x, Color::Highlight);
 
-    bool quit = false;
-
-    while (!quit) {
+    while (!client.quit) {
         switch (auto ch = getch()) {
-        case ctrl('c'):
-            quit = true;
-            break;
-        default:
-            break;
+            case ctrl('c'):
+                client.quit = true;
+                break;
+            case '\n': // Handle Error Key
+                client.chat.put(prompt.getPromptString());
+                prompt.clear();
+                break;
+            default:
+                if (std::isprint(ch)) {
+                    prompt.put(ch);
+                }
         }
+
+        status_bar("CppChat", 0, 0, x, Color::Highlight);
+        client.chat.render(0, 1);
+        status_bar("Offline", 0, y - 2, x, Color::Highlight);
+        prompt.render(0, y - 1, x);
+
         refresh();
     }
 
